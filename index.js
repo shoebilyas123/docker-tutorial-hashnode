8c8cb8e05ea92597ee526e3fb812f65f2b491574
shoebilyas123 <shoebilyas123@gmail.com>
2023-04-12 20:43:18 +0530

    init: docker nodeapp

---
+++ index.js
@@ -0,0 +1,23 @@
+// Imported modules
+const express = require('express');
+
+// Variables
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+    <head>
+      <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+     <h1>Hello World.  This is a Nodejs app.</h1>
+    </body>
+  </html>
+  `);
+});
+
+app.listen(8000, () => {
+  console.log(' Server is running on PORT 8000');
+});
